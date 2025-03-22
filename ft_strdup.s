f3b9a225939b2d3d147c36ab947314c1aa36eb89
sebamiro <seba.mi.21@gmail.com>
2025-03-22 12:45:31 +0100

    fix: strdup

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,18 +1,20 @@
-GLOBAL ft_strdup;
+section .note.GNU-stack
+global ft_strdup;
 extern ft_strlen
 extern malloc
 section .text
 ft_strdup:
 	call ft_strlen
-	mov r8, rdi
+	push rdi
 	mov rdi, rax
 	call malloc
+	pop rdi
 	xor r9, r9
 	jmp L10
 L9:
 	inc r9
 L10:
-	mov dl, [r8+r9]
+	mov dl, [rdi+r9]
 	mov [rax+r9], dl
 	cmp dl, 0
 	jne L9
